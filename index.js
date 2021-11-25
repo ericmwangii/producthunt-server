ca88b2f373b8685d3258d41c0fc857972b6999ce
ericmwangii <ericmwangii101@gmail.com>
2021-11-25 09:32:20 +0300

    deploy

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use("/", api);
 
 app.listen(PORT, () => {
